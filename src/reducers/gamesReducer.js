e032585ea5d1f387e748cf9fcca351a50ee23354
Dominic Varner <dommy@MLBs-Lappy.local>
2020-12-08 12:14:46 -0800

    setup redux, reducers to hold data, confirmed to work on chrome

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,18 @@
+// define what data will look like
+
+const initState = {
+    popular: [],
+    newGames: [],
+    upcoming: []
+}
+
+const gamesReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'FETCH_GAMES':
+            return { ...state }
+        default:
+            return { ...state }
+    }
+}
+
+export default gamesReducer
