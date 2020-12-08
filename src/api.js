b90931c4997e3c3e6279db07c1891136a2f9f399
Dominic Varner <dommy@MLBs-Lappy.local>
2020-12-08 10:45:28 -0800

    getting dates

---
+++ src/api.js
@@ -0,0 +1,33 @@
+// Base url
+const base_url = 'https://api.rawgio/api/games';
+
+// get month
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if (month < 10) {
+        return `0${month}`
+    } else {
+        return month
+    }
+};
+
+// get day
+const getCurrentDay = () => {
+    const month = new Date().getDate() + 1;
+    if (day < 10) {
+        return `0${day}`
+    } else {
+        return day
+    }
+};
+
+// current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentMonth}/${currentDay}/${currentYear}`;
+const lastYear = `${currentMonth}/${currentDay}/${currentYear - 1}`;
+const nextYear = `${currentMonth}/${currentDay}/${currentYear + 1}`;
+
+// Popular games
+const popular_games = 'https://api.rawg.io/api/games?dates=2020'
